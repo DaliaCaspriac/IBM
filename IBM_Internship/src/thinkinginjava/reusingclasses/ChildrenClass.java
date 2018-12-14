869831f03115a847e0081f0b1af4f2fc262fe0fd
Dalia Caspriac <dalia.caspriac@ibm.com>
2018-12-14 14:50:43 +0200

    Added solution to Exercise13

---
+++ IBM_Internship/src/thinkinginjava/reusingclasses/ChildrenClass.java
@@ -0,0 +1,7 @@
+package thinkinginjava.reusingclasses;
+
+public class ChildrenClass extends ParentClass {
+    public void doSomething(char c) {
+        System.out.println("char");
+    }
+}
--- IBM_Internship/src/thinkinginjava/reusingclasses/Main.java
+++ IBM_Internship/src/thinkinginjava/reusingclasses/Main.java
@@ -18,5 +18,15 @@ public class Main {
         } finally {
             stem.dispose();
         }
+
+        //Exercise 13: (2) Create a class with a method that is overloaded three times.
+        // Inherit a new class, add a new overloading of the method, and show that all
+        // four methods are available in the derived class.
+        System.out.println("-----Exercise 13------");
+        ChildrenClass childrenClass = new ChildrenClass();
+        childrenClass.doSomething('a');
+        childrenClass.doSomething(5);
+        childrenClass.doSomething(20.9f);
+        childrenClass.doSomething(44.27d);
     }
 }
