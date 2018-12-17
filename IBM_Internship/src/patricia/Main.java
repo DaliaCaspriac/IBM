bd022bbb903a653ddc3ab5bdefb2ea12be4a8d3f
Dalia Caspriac <dalia.caspriac@ibm.com>
2018-12-17 11:19:24 +0200

    Added solution to exercise2

--- IBM_Internship/src/patricia/Main.java
+++ IBM_Internship/src/patricia/Main.java
@@ -6,9 +6,30 @@ public class Main {
         // Make it have two constructors, one with both firstname and surname,
         // and one with full name (we assume each individual has only one surname
         // and at least one firstname separated by spaces). Test in main class.
+        System.out.println("---------Ex1--------");
         Person person1 = new Person("dalia","caspriac");
         Person person2 = new Person("ioana dalia caspriac");
         System.out.println(person1.toString());
         System.out.println(person2.toString());
+
+        //Create a class called Tank that can be filled and emptied
+        // (let’s say it has acts like a stack), and has a termination condition
+        // that it must be empty when the object is cleaned up.
+        // Write a finalize() that verifies this termination condition.
+        // In main(), test the possible scenarios that can occur when your Tank is used.
+        System.out.println("--------Ex 2---------");
+
+        Tank tank1 = new Tank();
+        tank1.add(new Integer(2));
+        tank1.add(new Integer(5));
+
+        new Tank().add(10);
+
+        Tank tank2 = new Tank();
+        tank2.add(new Integer(3));
+        tank2.add(new Integer(9));
+        tank2.empty();
+
+        System.gc();
     }
 }
--- IBM_Internship/src/patricia/Person.java
+++ IBM_Internship/src/patricia/Person.java
@@ -25,7 +25,7 @@ public class Person {
         }
         this.firstname = fName;
     }
-    
+
     @Override
     public String toString() {
         return "Person{" +
