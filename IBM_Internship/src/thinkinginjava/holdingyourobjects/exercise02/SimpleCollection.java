b3dcc0e81534b93de02a44e50af16688b9323c38
Dalia Caspriac <dalia.caspriac@ibm.com>
2018-12-19 10:49:43 +0200

    Added solution to Exercise 2 - basic concepts

---
+++ IBM_Internship/src/thinkinginjava/holdingyourobjects/exercise02/SimpleCollection.java
@@ -0,0 +1,18 @@
+package thinkinginjava.holdingyourobjects.exercise02;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SimpleCollection {
+
+    public static void main(String[] args) {
+        Set<Integer> c = new HashSet<>();
+        for(int i = 0; i < 10; i++){
+            c.add(i);
+        }
+        for(Integer i : c){
+            System.out.print(i + ", ");
+        }
+    }
+}
