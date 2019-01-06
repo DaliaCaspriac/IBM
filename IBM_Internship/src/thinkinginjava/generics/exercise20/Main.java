fc0d1ca1260802514e3d157140813c89c75f6d67
Dalia Caspriac <dalia.caspriac@ibm.com>
2019-01-06 11:15:10 +0200

    Added solution to Exercise 20 - Generics/The C++ approach

---
+++ IBM_Internship/src/thinkinginjava/generics/exercise20/Main.java
@@ -0,0 +1,8 @@
+package thinkinginjava.generics.exercise20;
+
+
+public class Main {
+    public static void main(String[] args) {
+        GenericClass.genericMethod(new MyClass());
+    }
+}
---
+++ IBM_Internship/src/thinkinginjava/generics/exercise20/MyClass.java
@@ -0,0 +1,17 @@
+package thinkinginjava.generics.exercise20;
+
+public class MyClass implements MyInterface {
+    @Override
+    public void f() {
+        System.out.println("f method");
+    }
+
+    @Override
+    public void g() {
+        System.out.println("g method");
+    }
+
+    public void h() {
+        System.out.println("h method");
+    }
+}
